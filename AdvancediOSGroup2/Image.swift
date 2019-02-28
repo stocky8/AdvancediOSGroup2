cd1ddcb70ed0f35990a553ef17148de21de72ae7
Benjamin Miles <dootbin@benjamins-mbp.cvtc.local>
2019-02-28 13:28:31 -0600

    removed image.swift

---
+++ AdvancediOSGroup2/Image.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  AdvancediOSGroup2
+//
+//  Created by Benjamin Miles on 2/27/19.
+//  Copyright © 2019 Benjamin Miles. All rights reserved.
+//
+
+import UIKit
+
+class Image: NSObject {
+
+}
